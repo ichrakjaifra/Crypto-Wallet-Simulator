731b33d45055bc298d9fae1413488a08fe090aee
ichrakjaifra <ichrak.jaifra@gmail>
2025-09-26 16:22:08 +0100

    CWS-15 <Implémentation du système de logging et gestion d'exceptions>

---
+++ src/main/java/com/crypto/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,11 @@
+package main.java.com.crypto.exceptions;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+
+    public InsufficientBalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/crypto/exceptions/InvalidAmountException.java
@@ -0,0 +1,11 @@
+package main.java.com.crypto.exceptions;
+
+public class InvalidAmountException extends RuntimeException {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+
+    public InvalidAmountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
